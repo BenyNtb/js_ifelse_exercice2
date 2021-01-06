d0a960ff4bb6d83bb7efe3e2133e5e25b80813ef
Beny <b.ntambwe97@gmail.com>
2021-01-06 14:31:10 +0100

    almost done

---
+++ public/js/main.js
@@ -0,0 +1,54 @@
+// EXO 1
+
+// let quest = prompt('Mettez un nombre')
+// let quest2 = prompt('Mettez un nombre')
+// console.log(quest,quest2);
+// if (quest == quest2) {
+//     alert(`le nombre ${quest} et ${quest2} sont-ils égaux?`)
+//     alert("oui")
+// } 
+// EXO 2
+
+// if (quest < quest2) {
+//     alert(`le nombre ${quest} est-il plus petit que ${quest2} ?`)
+//     alert(`oui`)
+// } else {
+//     alert(`le nombre ${quest} est-il plus petit que ${quest2} ?`)
+//     alert("non")
+// }
+
+// EXO 3
+
+// let prems = parseInt(prompt ('Mettez un premier nombre'))
+// let dems = parseInt(prompt ('Mettez un second nombre'))
+// let trems = parseInt(prompt ('Mettez un troisième nombre'))
+// console.log(prems + dems);
+// if (prems + dems == trems) {
+//     alert(`la somme du nombre ${prems} + le nombre ${dems} est-elle plus grande que le nombre ${trems} ? oui`)
+// } else {
+//     alert(`la somme du nombre ${prems} + le nombre ${dems} est-elle plus grande que le nombre ${trems} ? non`)
+// }
+// EXO 4
+
+// let pro = prompt('Tapez ici')
+// console.log(pro.length);
+// let result = pro.length
+// alert('Merci de votre réponse!')
+// let pro2 = prompt('Combien de caractères pensez-vous avoir dans votre phrase ?')
+// if (pro2 == result) {
+//     alert(`Bravo! il y'a bien ${result} caractères!`)
+// } else {
+//     alert(`Dommage! Tu n'étais qu'à ${Math.abs(result - pro2)} unités de la bonne réponse`)
+// }
+
+// EXO 5
+
+// let prenom = prompt ('Veuillez introduire votre nom')
+// if (prenom == 0) {
+//     ask = prompt('Attention vous devez remplir le champs ci dessous, Quel est votre prénom ?')
+// } else {
+//     alert (`Bonjour ${prenom}`)
+// }
+
+// EXO 6
+let pack = prompt ('Bienvenu(e) chez Beny Inc.')
